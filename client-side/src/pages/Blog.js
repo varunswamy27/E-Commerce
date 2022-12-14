a17d2500dac0db4b5791bb077d065352ba40d504
jomkar55 <99981958+jomkar55@users.noreply.github.com>
2022-12-15 01:44:02 +0530

    blog

--- client-side/src/pages/Blog.js
+++ client-side/src/pages/Blog.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import styles from "../styles/pages/Blog.module.scss";
+import img1 from "../img/blog/image_1.jpg"
+import { BiCalendar } from "react-icons/bi";
+import { BiRightArrowAlt } from "react-icons/bi";
+
 const Blog = () => {
     return (
 
@@ -9,27 +13,40 @@ const Blog = () => {
             <div className={`${styles.container_sec} container`} >
 
                 <div className={styles.blog_flex}>
-                    <div style={{ border: "2px solid black", width: "50%" }} className={styles.grids}>
 
-                    </div>
-                    <div style={{ border: "2px solid black", width: "50%" }}>
 
-                    </div>
+                    <div style={{ border: "2px solid black" }} className={styles.grids}>
+                        <div style={{ border: "2px solid black" }}>
+                            <img src={img1} alt='wines' />
+                        </div>
+                        <div style={{ border: "2px solid black" }} className={styles.grid_sec_two}>
 
-                    {/* <div className={styles.demo}>
-                        <div className='container'>
-
-                            <div className={styles.demo2}>
-
-                            </div>
-
+                            <p className={styles.grid_top_calender}><span><BiCalendar /></span> 23 April 2020</p>
+                            <p className={styles.grid_heading}>The Recipe from a Winemaker’s Restaurent</p>
+                            <p className={styles.grid_para}>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                            <button> continue <BiRightArrowAlt /> </button>
                         </div>
 
-                    </div> */}
+                    </div>
+
+                    <div style={{ border: "2px solid black" }} className={styles.grids}>
+
+
+
+                    </div>
+
+
                 </div>
-                </div>
-                </section>
-                );
+
+
+
+
+
+
+
+            </div>
+        </section>
+    );
 };
 
-                export default Blog;
+export default Blog;
