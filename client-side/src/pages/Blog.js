379ab44324f5718278799613c512b787061a3a24
varunswamy27 <varunswamy27@gmail.com>
2022-12-14 10:29:10 +0530

    Port Changed

--- client-side/src/pages/Blog.js
+++ client-side/src/pages/Blog.js
@@ -3,15 +3,9 @@ import styles from "../styles/pages/Blog.module.scss";
 const Blog = () => {
     return (
         <div className={styles.demo}>
-            <div className='container' style={{border:"2px solid black"}}>
+            <div className='container'>
 
-                <div className={styles.blog_flex}>
-                    <div style={{border:"2px solid black" , width:"50%"}}>
-
-                    </div>
-                    <div style={{border:"2px solid black" , width:"50%"}}>
-
-                    </div>
+                <div className={styles.demo2}>
                 </div>
 
             </div>
