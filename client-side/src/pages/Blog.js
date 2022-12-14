d2374810c2d160e14533d912714a25ad353391c4
jomkar55 <99981958+jomkar55@users.noreply.github.com>
2022-12-14 19:31:18 +0530

    blog

--- client-side/src/pages/Blog.js
+++ client-side/src/pages/Blog.js
@@ -2,11 +2,13 @@ import React from 'react';
 import styles from "../styles/pages/Blog.module.scss";
 const Blog = () => {
     return (
-        <div className={styles.demo}>
-            <div className='container' style={{border:"2px solid black"}}>
+        
+        <section className={styles.blog_section}>
+     
+            <div className={`${styles.container_sec} container`} >
 
                 <div className={styles.blog_flex}>
-                    <div style={{border:"2px solid black" , width:"50%"}}>
+                    <div style={{border:"2px solid black" , width:"50%"}} className={styles.grids}>
 
                     </div>
                     <div style={{border:"2px solid black" , width:"50%"}}>
@@ -16,7 +18,8 @@ const Blog = () => {
 
             </div>
             
-        </div>
+     
+        </section>
     );
 };
 
