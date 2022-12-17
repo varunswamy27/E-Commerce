7eda4538e803a9863ab83b61a141889691ccab98
varunswamy27 <varunswamy27@gmail.com>
2022-12-17 15:02:52 +0530

    Made Common Banner

--- client-side/src/pages/Home.js
+++ client-side/src/pages/Home.js
@@ -2,11 +2,13 @@ import React from "react";
 import BannerTag from "../components/BannerTag";
 import Desire from "../components/Desire";
 import ProductCircle from "../components/ProductCircle";
+import CommanBanner from "../components/CommanBanner";
 import styles from "../styles/pages/Home.module.scss";
 
 const Home = () => {
   return (
     <div className={styles.home_page}>
+      <CommanBanner title="Home" />
       <BannerTag />
       <Desire />
       <ProductCircle />
