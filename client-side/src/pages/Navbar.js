9ba7f337b3054f3d46d179485a2de59612190d55
varunswamy27 <varunswamy27@gmail.com>
2022-12-17 22:50:42 +0530

    Added Logo

--- client-side/src/pages/Navbar.js
+++ client-side/src/pages/Navbar.js
@@ -4,29 +4,39 @@ import styles from "../styles/pages/Navbar.module.scss";
 import ScrollPosition from "../Hooks/ScrollPosition";
 
 const Navbar = () => {
-
-const scrollPosition = ScrollPosition();
-// console.log(scrollPosition);
+  const scrollPosition = ScrollPosition();
+  // console.log(scrollPosition);
 
   return (
     <nav className={scrollPosition > 0 ? styles.scroll : null}>
-      <div className={scrollPosition > 0 ? `${styles.shadow} container` : `${styles.navbar} container`}>
+      <div
+        className={
+          scrollPosition > 0
+            ? `${styles.shadow} container`
+            : `${styles.navbar} container`
+        }
+      >
         <div className={styles.link_wrap}>
-          <Link to="/">
-            <p className={`${styles.link_name} text_xs`}>Home</p>
-          </Link>
-          <Link to="/about-us">
-            <p className={`${styles.link_name} text_xs`}>About</p>
-          </Link>
-          <Link to="/product">
-            <p className={`${styles.link_name} text_xs`}>Product</p>
-          </Link>
-          <Link to="/blog">
-            <p className={`${styles.link_name} text_xs`}>Blog</p>
-          </Link>
-          <Link to="/contact">
-            <p className={`${styles.link_name} text_xs`}>Contact</p>
-          </Link>
+          <div className={styles.logo_box}>
+            <p>LIQUOR <span>STORE</span></p>
+          </div>
+          <div className={styles.links}>
+            <Link to="/">
+              <p className={`${styles.link_name} text_xs`}>Home</p>
+            </Link>
+            <Link to="/about-us">
+              <p className={`${styles.link_name} text_xs`}>About</p>
+            </Link>
+            <Link to="/product">
+              <p className={`${styles.link_name} text_xs`}>Product</p>
+            </Link>
+            <Link to="/blog">
+              <p className={`${styles.link_name} text_xs`}>Blog</p>
+            </Link>
+            <Link to="/contact">
+              <p className={`${styles.link_name} text_xs`}>Contact</p>
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
