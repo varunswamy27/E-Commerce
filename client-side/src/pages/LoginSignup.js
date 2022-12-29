ab3e3e87c8e774ddcbf4a0f67a72d0488ee5f685
varunswamy27 <varunswamy27@gmail.com>
2022-12-29 17:54:14 +0530

    Changes in Login

--- client-side/src/pages/LoginSignup.js
+++ client-side/src/pages/LoginSignup.js
@@ -11,8 +11,46 @@ const LoginSignup = () => {
         <div className={styles.login_form}>
           <div className={styles.login_box}>
             {isSignUp ? <p>Login</p> : <p>Sign Up</p>}
-            <input type="text" />
-            <input type="text" />
+            {isSignUp ? (
+              <>
+                <div>
+                  <input type="text" placeholder="Enter Your Email" />
+                </div>
+                <div>
+                  <input type="text" placeholder="Enter Your Password" />
+                </div>
+                <div>
+                  <button>Log In</button>
+                </div>
+                <div>
+                  <p>Don't have an account <span onClick={()=>{setIsSignUp(false)}}>SignUp Here?</span></p>
+                </div>
+              </>
+            ) : (
+              <>
+                <div>
+                  <input type="text" placeholder="Enter Your First Name" />
+                </div>
+                <div>
+                  <input type="text" placeholder="Enter Your Last Name" />
+                </div>
+                <div>
+                  <input type="text" placeholder="Enter Your Email" />
+                </div>
+                <div>
+                  <input type="text" placeholder="Enter Your Password" />
+                </div>
+                <div>
+                  <input type="text" placeholder="Enter Your Number" />
+                </div>
+                <div>
+                  <button>Sign Up</button>
+                </div>
+                <div>
+                  <p>Already have an account <span onClick={()=>{setIsSignUp(true)}}>Login Here!</span></p>
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
