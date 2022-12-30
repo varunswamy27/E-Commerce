17fdc6f5d12e3ff673410e11da4b8e9ad8ce777d
varunswamy27 <varunswamy27@gmail.com>
2022-12-30 12:21:43 +0530

    Loader

---
+++ client-side/src/Hooks/Loader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+const Loader = () => {
+
+    useEffect(()=>{
+        
+    },[])
+  return (
+    <div>Loader</div>
+  )
+}
+
+export default Loader
