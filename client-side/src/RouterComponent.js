78e3498ebce6d5ff74e81fc3c5b226634e160542
varunswamy27 <varunswamy27@gmail.com>
2022-12-30 21:17:36 +0530

    Finished Auth Page

--- client-side/src/RouterComponent.js
+++ client-side/src/RouterComponent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import "./styles/globals/globals.scss";
 import Navbar from "./pages/Navbar";
 import Home from "./pages/Home";
@@ -8,28 +8,35 @@ import Blog from "./pages/Blog";
 import Contact from "./pages/Contact";
 import Product from "./pages/Product";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ProductSingle from './pages/ProductSingle';
-import LoginSignup from './pages/LoginSignup';
+import ProductSingle from "./pages/ProductSingle";
+import LoginSignup from "./pages/LoginSignup";
+import { useState, useEffect } from "react";
+
 function RouterComponent() {
-    return (
-        <div>
-            <BrowserRouter>
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about-us" element={<AboutUs />} />
-                    <Route path="/product" element={<Product />} />
-                    <Route path="/product-single" element={<ProductSingle />} />
-                    <Route path="/blog" element={<Blog />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="/auth" element={<LoginSignup />} />
-                    <Route path="*" element={<NoPage />} />
+  const pathLocation = window.location.pathname;
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(false);
+  }, [pathLocation]);
 
-                </Routes>
-            </BrowserRouter>
-
-        </div>
-    );
+  return (
+    <div>
+      {loading ? <p>Loading.......</p> : null}
+      <BrowserRouter>
+        {pathLocation === "/auth" ? null : <Navbar />}
+        <Routes>
+          <Route path="/auth" element={<LoginSignup />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/product/product-single" element={<ProductSingle />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default RouterComponent;
--- client-side/src/pages/LoginSignup.js
+++ client-side/src/pages/LoginSignup.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import styles from "../styles/pages/LoginSignup.module.scss";
+import { FcGoogle } from "react-icons/fc";
+import authImg from "../img/home/comman_banner.jpg"
 
 const LoginSignup = () => {
   const [isSignUp, setIsSignUp] = useState(false);
@@ -7,47 +9,88 @@ const LoginSignup = () => {
   return (
     <div className={styles.login_signup}>
       <div className={styles.login_wrap}>
-        <div className={styles.login_img}></div>
+        <div className={styles.login_img}>
+          <img className={styles.auth_img} src={authImg} alt="" />
+        </div>
         <div className={styles.login_form}>
           <div className={styles.login_box}>
-            {isSignUp ? <p>Login</p> : <p>Sign Up</p>}
+            {isSignUp ? (
+              <p className={`${styles.title} text_md`}>
+                <span className={styles.log}>Log</span>in
+              </p>
+            ) : (
+              <p className={`${styles.title} text_md`}>
+                <span className={styles.sign}>Sign</span> Up
+              </p>
+            )}
             {isSignUp ? (
               <>
-                <div>
+                <div className={`${styles.inp_box_log}`}>
                   <input type="text" placeholder="Enter Your Email" />
                 </div>
-                <div>
+                <div className={`${styles.inp_box_log}`}>
                   <input type="text" placeholder="Enter Your Password" />
                 </div>
-                <div>
-                  <button>Log In</button>
+                <div className={`${styles.btn_log_in}`}>
+                  <button>LOGIN</button>
                 </div>
-                <div>
-                  <p>Don't have an account <span onClick={()=>{setIsSignUp(false)}}>SignUp Here?</span></p>
+                <div className={`${styles.btn_google}`}>
+                  <button>
+                    Continue with Google <FcGoogle className={styles.google} />
+                  </button>
+                </div>
+                <div className={`${styles.account}`}>
+                  <p className="text_xs">
+                    Don't have an account
+                    <br />
+                    <span
+                      onClick={() => {
+                        setIsSignUp(false);
+                      }}
+                    >
+                      SignUp Here?
+                    </span>
+                  </p>
                 </div>
               </>
             ) : (
               <>
-                <div>
+                <div className={`${styles.inp_box_sign}`}>
                   <input type="text" placeholder="Enter Your First Name" />
                 </div>
-                <div>
+                <div className={`${styles.inp_box_sign}`}>
                   <input type="text" placeholder="Enter Your Last Name" />
                 </div>
-                <div>
+                <div className={`${styles.inp_box_sign}`}>
                   <input type="text" placeholder="Enter Your Email" />
                 </div>
-                <div>
+                <div className={`${styles.inp_box_sign}`}>
                   <input type="text" placeholder="Enter Your Password" />
                 </div>
-                <div>
+                <div className={`${styles.inp_box_sign}`}>
                   <input type="text" placeholder="Enter Your Number" />
                 </div>
-                <div>
-                  <button>Sign Up</button>
+                <div className={`${styles.btn_sign_up}`}>
+                  <button>SIGN UP</button>
                 </div>
-                <div>
-                  <p>Already have an account <span onClick={()=>{setIsSignUp(true)}}>Login Here!</span></p>
+                <div className={`${styles.btn_google}`}>
+                  <button>
+                    Continue with Google
+                    <FcGoogle className={styles.google} />
+                  </button>
+                </div>
+                <div className={`${styles.account}`}>
+                  <p className="text_xs">
+                    Already have an account
+                    <br />
+                    <span
+                      onClick={() => {
+                        setIsSignUp(true);
+                      }}
+                    >
+                      Login Here!
+                    </span>
+                  </p>
                 </div>
               </>
             )}
