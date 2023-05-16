2559e9e9145729760186ad4e4e537d0884d367da
varunswamy27 <varunswamy27@gmail.com>
2023-05-16 21:15:04 +0530

    Build

--- client-side/src/pages/Navbar.js
+++ client-side/src/pages/Navbar.js
@@ -85,7 +85,7 @@ const Navbar = () => {
               <p className={`${styles.logout} text_xs`} onClick={() => dispatch(logout())}>{user ? 'Logout' : null}</p>
             </Link>
             <Link to="/cart">
-              <p className={`${styles.link_name} text_xs`}>Cart <span className={styles.cartLength}>{cartLength === undefined ? `0` : cartLength}</span></p>
+              <p className={`${styles.cart_name} text_xs`}>Cart <span className={styles.cartLength}>{cartLength === undefined ? `0` : cartLength}</span></p>
             </Link>
             <button ref={btnRef} onClick={() => { setActive(!isActive) }} className={styles.hamburger}>{isActive ?
               <AiOutlineClose />
@@ -106,7 +106,9 @@ const Navbar = () => {
           <Link to="/contact">
             <p onClick={() => { setActive(false) }} className={`${styles.link_name} text_sm`}>Contact</p>
           </Link>
-          <p className={`${styles.link_name} text_xs`}>Logout</p>
+          <Link to="/auth">
+            <p className={`${styles.logout} text_sm`} onClick={() => dispatch(logout())}>{user ? 'Logout' : null}</p>
+          </Link>
         </div>
       </div>
     </nav>
